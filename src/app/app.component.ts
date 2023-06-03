12c34140af67f7c549a5a89368f5114a07184553
satyam sinha <42906814+satyamsinha111@users.noreply.github.com>
2023-06-03 21:56:37 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import heic2any from "heic2any";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app2';
+   public imageUrl:string = '';
+
+   constructor(){
+    //
+   }
+
+
+   onUploadHeicFile(event:any){
+
+   }
 }
